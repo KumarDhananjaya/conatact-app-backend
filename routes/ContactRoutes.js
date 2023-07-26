5ff4be249475b6991caa38025f6d3a289fa9810d
S Kumar Dhananjaya <KUMARDHANANJAYA.EDU@GMAIL.COM>
2023-07-26 15:46:16 +0530

    Multiple HTTP Methods per Route

--- routes/ContactRoutes.js
+++ routes/ContactRoutes.js
@@ -6,15 +6,9 @@ const { getContacts,
     updateContact,
     deleteContact} = require("../controllers/ContactController");
 
-router.route("/").get(getContacts);
+router.route("/").get(getContacts).post(createContact);
 
-router.route("/").post(createContact);
-
-router.route("/:id").get(getContact);
-
-router.route("/:id").put(updateContact);
-
-router.route("/:id").delete(deleteContact);
+router.route("/:id").get(getContact).put(updateContact).delete(deleteContact);
 
 
 module.exports = router;
