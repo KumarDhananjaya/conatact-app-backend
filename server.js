7fbf36c2bb9dbd5948838f6d33dfc01a7e252ca4
S Kumar Dhananjaya <KUMARDHANANJAYA.EDU@GMAIL.COM>
2023-07-26 15:02:28 +0530

    created express server with thunder client setup to test api

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("server started ");
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+
+const app = express();
+
+const port =  process.env.PORT || 3000;
+
+app.get("/api/contacts", (req, res) => {
+    res.status(200).json({mesagge: "Get all contacts"});
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port : ${port}`);
+});
