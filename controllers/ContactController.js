153c28b60bb99b1bdb2d2ece3669829577b0ace3
S Kumar Dhananjaya <KUMARDHANANJAYA.EDU@GMAIL.COM>
2023-07-27 14:55:00 +0530

    update contact using id

--- controllers/ContactController.js
+++ controllers/ContactController.js
@@ -44,7 +44,18 @@ const getContact = asyncHandler(async (req, res) => {
 //@route PUT /api/contacts/:id
 //@access public
 const updateContact = asyncHandler(async (req, res) => {
-    res.status(200).json({mesagge: `Update contact for ${req.params.id}`});
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error[" Contact Not Found "]
+    }
+
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id, 
+        req.body, 
+        {new: true},
+    )
+    res.status(200).json(updatedContact);
 });
 
 //@desc delete Contact
